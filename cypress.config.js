0d51615bec8e49e2a5fd407fd63af7cb79980a3a
vinicius santos <viniciusgoncalves1101@gmail.com>
2025-07-01 22:00:27 -0300

    Criacao cts caseWeb

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+
+module.exports = defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)],
+        })
+      );
+
+      return config;
+    },
+    specPattern: '**/*.feature',
+    //baseUrl: 'http://localhost:3000', // ajuste conforme seu projeto
+  },
+});
---
+++ cypress/e2e/step_definitions/caseWebStep.js
@@ -0,0 +1,16 @@
+const {When, Then, Given} = require("@badeball/cypress-cucumber-preprocessor")
+const caseWebPage = require('../../pages/caseWebPage')
+
+
+Given("acesso o site", () => {
+    caseWebPage.acessarUrl()
+})
+
+When ("clico nos 3 botoes principais apresentados na tela", () => {
+    caseWebPage.clicarNos3BotoesPrincipais()
+})  
+
+
+When ("clico em todos os botoes de edit e delet da grid", () => {
+    caseWebPage.clicarEmTodosEditDeletDaGrid()
+}) 
